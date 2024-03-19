1a6a0617252395a78e2556c496528bdad0b4e971
farmer-Beixu <109709911+farmer-Beixu@users.noreply.github.com>
2024-03-19 17:23:23 +0800

    Add files via upload

---
+++ src/settings.f5077.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/main.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"1b2f5",main:"cd7eb"}};
