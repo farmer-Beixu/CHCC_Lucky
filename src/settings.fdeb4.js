0b880ce9f21d46d8a65697f4d55629d700fc5d65
farmer-Beixu <109709911+farmer-Beixu@users.noreply.github.com>
2024-03-28 17:17:40 +0800

    Add files via upload

---
+++ src/settings.fdeb4.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default","UI2D"],collisionMatrix:[[false],[false,true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/main.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"5969c",main:"386d6"}};
